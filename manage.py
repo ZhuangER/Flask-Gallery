9031fe0d939e8e1b200e2f59515ed8aa15c78b6f
ZhuangEr <huangyu.wuhan@gmail.com>
2016-02-10 17:12:39 -0500

    change port

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role, Photo=Photo)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-manager.add_command("runserver", Server(port=5000, host='0.0.0.0'))
+manager.add_command("runserver", Server(host='0.0.0.0'))
 
 
 @manager.command
