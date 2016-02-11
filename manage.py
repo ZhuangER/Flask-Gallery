09a0e44d2ccc29197503a10939dfbcdd402070a5
ZhuangEr <huangyu.wuhan@gmail.com>
2016-02-10 22:51:12 -0500

    modify profile.html

--- manage.py
+++ manage.py
@@ -19,13 +19,6 @@ manager.add_command('db', MigrateCommand)
 manager.add_command("runserver", Server(host='0.0.0.0'))
 
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
 
 if __name__ == '__main__':
     manager.run()
